8b5ca50c39f859a65463e17a61f7508a1260fe6c
Andy Ambrosio <aambrosio-2021105@kinal.edu.gt>
2023-01-20 08:59:40 -0600

    primer commit v1

---
+++ js/index.js
@@ -0,0 +1,15 @@
+
+function saludar(){
+    console.log('hola mundo');
+}
+
+function sumar(){
+    console.log('hola mundo');
+    let num1 =document.getElementById("num1").value;
+    let num2 =document.getElementById("num2").value;
+    
+    let resultado= parseInt(num1)+parseInt(num2);
+    console.log(num1);
+    console.log(num2);
+    console.log(resultado);
+}
